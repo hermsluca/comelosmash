2af904cea7dfe4bb712473cd5b1ca77ae88ef199
Luca Herms <herms_luca@web.de>
2026-08-28 12:51:13 +0200

    Remove unused API response handling from fetch function in index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@
 
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-
-    if (url.pathname.startsWith("/api/")) {
-      return new Response(JSON.stringify({ name: "Cloudflare" }), {
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-
     return env.ASSETS.fetch(request);
   },
 };
